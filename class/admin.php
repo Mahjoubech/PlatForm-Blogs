35bfd5e7b3cce401496d53de3f1ebd3ea07d99aa
Mahjoubech <charkaouielmahjoub50@gmail.com>
2025-01-06 01:22:35 +0100

    add class admin heritant from user

---
+++ class/admin.php
@@ -0,0 +1,13 @@
+<?php 
+// class User{
+//     private $cnx;
+//     public function __construct($cnx){
+//         $this->cnx = $cnx;  
+//     }
+
+//     public function getAllClients() {
+//         $stmt = $this->cnx->query("SELECT * FROM user");
+//         return $stmt->fetchAll(PDO::FETCH_ASSOC);
+//     }
+// }
+?>
