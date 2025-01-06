05b7ccbdde3e3227bf308c83b609f63f3c9c89f7
Mahjoubech <charkaouielmahjoub50@gmail.com>
2025-01-06 01:23:13 +0100

    add class client heritant from user and user function register

---
+++ class/Client.php
@@ -0,0 +1,42 @@
+<?php 
+require_once 'User.php';
+class Client extends User{
+    public function login($email, $password): bool {
+        $stmt = $this->cnx->prepare("SELECT * FROM users WHERE email = ? AND role = 2");
+        $stmt->execute([$email]);
+        $client = $stmt->fetch();
+
+        if ($client && $this->verifyPassword($password, $client['password'])) {
+            $this->createSession($client);
+            return true;
+        }
+        return false;
+    }
+
+  public function register($username, $email, $password, $role) {
+    $stmt = $this->cnx->prepare("SELECT * FROM user WHERE email = :email");
+    $stmt->bindParam(':email', $email, PDO::PARAM_STR);
+    $stmt->execute();
+    if ($stmt->rowCount() > 0) {
+        $_SESSION['emaildija'] = "Cet email est déjà utilisé. Veuillez utiliser un autre email.";
+        header("Location: ../Register.php");
+        exit();
+    }
+
+        $hashedPassword = password_hash($password, PASSWORD_BCRYPT); // Hachage du mot de passe
+        $stmt = $this->cnx->prepare("INSERT INTO user (username, email, password, role_id) VALUES (:username, :email, :password, :role)");
+        $stmt->bindParam(':username', $username, PDO::PARAM_STR);
+        $stmt->bindParam(':email', $email, PDO::PARAM_STR);
+        $stmt->bindParam(':password', $hashedPassword, PDO::PARAM_STR);
+        $stmt->bindParam(':role', $role, PDO::PARAM_STR);
+        if ($stmt->execute()) {
+            $_SESSION['ressie'] = "Inscription réussie !";
+            return  $_SESSION['ressie'];
+        } else {
+           $_SESSION['errrinsc'] =  throw new Exception("Erreur lors de l'inscription. Veuillez réessayer.");
+        }
+    }   
+}
+
+?>
+
