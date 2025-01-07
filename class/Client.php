39c546bbcc1470b85ad80faa51cb614ce23dd14a
Mahjoubech <charkaouielmahjoub50@gmail.com>
2025-01-07 04:06:08 +0100

    add function in class client

--- class/Client.php
+++ class/Client.php
@@ -1,27 +1,35 @@
-<?php 
+<?php
 require_once 'User.php';
-class Client extends User{
-    public function login($email, $password): bool {
-        $stmt = $this->cnx->prepare("SELECT * FROM users WHERE email = ? AND role = 2");
+class Client extends User
+{
+    public function login($email, $password): bool
+    {
+        $stmt = $this->cnx->prepare("SELECT * FROM user WHERE email = ?");
         $stmt->execute([$email]);
         $client = $stmt->fetch();
-
-        if ($client && $this->verifyPassword($password, $client['password'])) {
+        var_dump($client["role_id"]);
+        if ($client) {
+        if ($this->verifyPassword($password, $client['password']) && $client['role_id'] == 2) {
+            header('location: .././views/client.php');
             $this->createSession($client);
+            } else {
+                header('location: .././views/admin.php');
+                $this->createSession($client);
+            }
             return true;
-        }
-        return false;
     }
 
-  public function register($username, $email, $password, $role) {
-    $stmt = $this->cnx->prepare("SELECT * FROM user WHERE email = :email");
-    $stmt->bindParam(':email', $email, PDO::PARAM_STR);
-    $stmt->execute();
-    if ($stmt->rowCount() > 0) {
-        $_SESSION['emaildija'] = "Cet email est déjà utilisé. Veuillez utiliser un autre email.";
-        header("Location: ../Register.php");
-        exit();
     }
+    public function register($username, $email, $password, $role)
+    {
+        $stmt = $this->cnx->prepare("SELECT * FROM user WHERE email = :email");
+        $stmt->bindParam(':email', $email, PDO::PARAM_STR);
+        $stmt->execute();
+        if ($stmt->rowCount() > 0) {
+            $_SESSION['emaildija'] = "Cet email est déjà utilisé. Veuillez utiliser un autre email.";
+            header("Location: ../Register.php");
+            exit();
+        }
 
         $hashedPassword = password_hash($password, PASSWORD_BCRYPT); // Hachage du mot de passe
         $stmt = $this->cnx->prepare("INSERT INTO user (username, email, password, role_id) VALUES (:username, :email, :password, :role)");
@@ -33,10 +41,7 @@ class Client extends User{
             $_SESSION['ressie'] = "Inscription réussie !";
             return  $_SESSION['ressie'];
         } else {
-           $_SESSION['errrinsc'] =  throw new Exception("Erreur lors de l'inscription. Veuillez réessayer.");
+            $_SESSION['errrinsc'] =  throw new Exception("Erreur lors de l'inscription. Veuillez réessayer.");
         }
-    }   
+    }
 }
-
-?>
-
