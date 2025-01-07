db250ea03b29d3d2de7e217beb00dd8420541c27
Mahjoubech <charkaouielmahjoub50@gmail.com>
2025-01-08 00:23:53 +0100

    chnage in user page delete fun session

--- class/User.php
+++ class/User.php
@@ -16,12 +16,5 @@ session_start();
          session_destroy();
          session_unset();
      }
- 
-     protected function createSession($userData): void {
-         session_start();
-         $_SESSION['user_id'] = $userData['id'];
-         $_SESSION['role'] = $userData['role'];
-         $_SESSION['username'] = $userData['username'];
-     }
 }
 ?>
