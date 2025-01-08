478fd1c27c35c732d26675bd8462d11d2218d2c2
Mahjoubech <charkaouielmahjoub50@gmail.com>
2025-01-08 14:52:43 +0100

    add admin

--- class/admin.php
+++ class/admin.php
@@ -1,13 +1,6 @@
 <?php 
-// class User{
-//     private $cnx;
-//     public function __construct($cnx){
-//         $this->cnx = $cnx;  
-//     }
-
-//     public function getAllClients() {
-//         $stmt = $this->cnx->query("SELECT * FROM user");
-//         return $stmt->fetchAll(PDO::FETCH_ASSOC);
-//     }
-// }
+require_once './User.php';
+class Client extends User{
+  
+}
 ?>
