665e650a42368a4be5aab9ce85404b3fef583f52
Mahjoubech <charkaouielmahjoub50@gmail.com>
2025-01-09 15:25:13 +0100

    change logique in class client

--- class/Client.php
+++ class/Client.php
@@ -1,50 +1,54 @@
 <?php
 require_once 'User.php';
-class Client extends User
-{
-    public function login($email, $password): bool
-    {
+class Client extends User {
+    public function login($email, $password): bool {
         $stmt = $this->cnx->prepare("SELECT * FROM user WHERE email = ?");
         $stmt->execute([$email]);
         $client = $stmt->fetch();
-        var_dump($client["role_id"]);
-        if ($client) {
-        if ($this->verifyPassword($password, $client['password']) && $client['role_id'] == 2) {
-            $_SESSION['user'] = $client;
 
-            header('location: .././views/client.php');
-           
+        if ($client) {
+            if ($this->verifyPassword($password, $client['password']) && $client['role_id'] == 2) {
+                $this->setUsername($client['username']);
+                $this->setEmail($client['email']);
+                $this->setRole($client['role_id']);
+
+                $_SESSION['user'] = $client;
+                header('Location: .././views/client.php');
             } else {
                 $_SESSION['user'] = $client;
-
-                header('location: .././views/admin/article.php');
+                header('Location: .././views/admin/article.php');
             }
             return true;
+        }
+
+        return false; 
     }
 
-    }
-    public function register($username, $email, $password, $role)
-    {
+    public function register($username, $email, $password, $role) {
         $stmt = $this->cnx->prepare("SELECT * FROM user WHERE email = :email");
         $stmt->bindParam(':email', $email, PDO::PARAM_STR);
         $stmt->execute();
+
         if ($stmt->rowCount() > 0) {
             $_SESSION['emaildija'] = "Cet email est déjà utilisé. Veuillez utiliser un autre email.";
             header("Location: ../Register.php");
             exit();
         }
-
-        $hashedPassword = password_hash($password, PASSWORD_BCRYPT); // Hachage du mot de passe
+        $this->setUsername($username);
+        $this->setEmail($email);
+        $this->setPassword($password);
+        $this->setRole($role);
         $stmt = $this->cnx->prepare("INSERT INTO user (username, email, password, role_id) VALUES (:username, :email, :password, :role)");
-        $stmt->bindParam(':username', $username, PDO::PARAM_STR);
-        $stmt->bindParam(':email', $email, PDO::PARAM_STR);
-        $stmt->bindParam(':password', $hashedPassword, PDO::PARAM_STR);
-        $stmt->bindParam(':role', $role, PDO::PARAM_STR);
+        $stmt->bindParam(':username', $this->getUsername(), PDO::PARAM_STR);
+        $stmt->bindParam(':email', $this->getEmail(), PDO::PARAM_STR);
+        $stmt->bindParam(':password', $this->getPassword(), PDO::PARAM_STR);
+        $stmt->bindParam(':role', $this->getRole(), PDO::PARAM_STR);
+
         if ($stmt->execute()) {
             $_SESSION['ressie'] = "Inscription réussie !";
-            return  $_SESSION['ressie'];
+            return $_SESSION['ressie'];
         } else {
-            $_SESSION['errrinsc'] =  throw new Exception("Erreur lors de l'inscription. Veuillez réessayer.");
+            throw new Exception("Erreur lors de l'inscription. Veuillez réessayer.");
         }
     }
 }
