763cd978f74e579c9b93d520f61066048504536f
Mahjoubech <charkaouielmahjoub50@gmail.com>
2025-01-10 15:23:15 +0100

    change class article

--- class/Article.php
+++ class/Article.php
@@ -1,14 +1,61 @@
 <?php
 class Article {
     private $cnx;
-
-    // Composition : Liste des commentaires et des likes
-    private $comments = [];
-    private $likes = [];
-
+    private $id;
+    private $userId;
+    private $title;
+    private $content;
+    private $image;
+    private $categoryId;
+    // private $comments = [];
+    // private $likes = [];
     public function __construct($db) {
         $this->cnx = $db;
     }
+    public function getId() {
+        return $this->id;
+    }
+
+    public function setId($id) {
+        $this->id = $id;
+    }
+    public function getUserId() {
+        return $this->userId;
+    }
+
+    public function setUserId($userId) {
+        $this->userId = $userId;
+    }
+    public function getTitle() {
+        return $this->title;
+    }
+
+    public function setTitle($title) {
+        $this->title = $title;
+    }
+    public function getContent() {
+        return $this->content;
+    }
+
+    public function setContent($content) {
+        $this->content = $content;
+    }
+    public function getImage() {
+        return $this->image;
+    }
+
+    public function setImage($image) {
+        $this->image = $image;
+    }
+    public function getCategoryId() {
+        return $this->categoryId;
+    }
+
+    public function setCategoryId($categoryId) {
+        $this->categoryId = $categoryId;
+    }
+
+    // Method to fetch all articles
     public function getAllArticles() {
         $sql = $this->cnx->prepare('
             SELECT article.*, user.username as name, category.name as catname
@@ -17,63 +64,31 @@ class Article {
             JOIN category ON article.categId = category.catId
         ');
         $sql->execute();
-       return $sql->fetchAll(PDO::FETCH_ASSOC);
+        return $sql->fetchAll(PDO::FETCH_ASSOC);
     }
-    //creat art
+
+    // Create an article
     public function create($userId, $title, $content, $image, $categoryId) {
         $stmt = $this->cnx->prepare("INSERT INTO article (userId, title, content, image, categId) 
             VALUES (?, ?, ?, ?, ?)");
-        $stmt->execute([$userId, $title, $content, $image, $categoryId]);
-       $id = $this->cnx->lastInsertId(); 
-        return $id;
+        return $stmt->execute([$this->setUserId($userId), $this->setTitle($title), $content, $image, $categoryId]);
     }
-    //  // Update an article
-    //  public function update($id) {
-    //     if ($id) {
-    //         $stmt = $this->cnx->prepare("UPDATE article SET title = ?, content = ?, image = ?, categId = ? WHERE art_Id = ?");
-    //         $stmt->execute([$this->title, $this->content, $this->image, $this->categoryId, $id]);
-    //         return true;
-    //     }
-    //     return false;
-    // }
-    //  // Delete an article
-    //  public function delete($id) {
-    //     if ($this->id) {
-    //         $stmt = $this->cnx->prepare("DELETE FROM article WHERE art_Id = ?");
-    //         $stmt->execute([$this->id]);
-    //         return true;
-    //     }
-    //     return false;
-    // }
-    // // Méthode pour charger les commentaires
-    // public function loadComments($db) {
-    //     $stmt = $db->prepare("SELECT * FROM comments WHERE article_id = ?");
-    //     $stmt->execute([$this->id]);
-    //     $results = $stmt->fetchAll();
 
-    //     foreach ($results as $row) {
-    //         $this->comments[] = new Comment($row['cmmlId'], $row['article_id'], $row['user_id'], $row['visit_name'], $row['cmnter'], $row['created_at']);
-    //     }
-    // }
+    // Update an article
+    public function update() {
+        if ($this->id) {
+            $stmt = $this->cnx->prepare("UPDATE article SET title = ?, content = ?, image = ?, categId = ? WHERE art_Id = ?");
+            return $stmt->execute([$this->title, $this->content, $this->image, $this->categoryId, $this->id]);
+        }
+        return false;
+    }
 
-    // // Méthode pour charger les likes
-    // public function loadLikes($db) {
-    //     $stmt = $db->prepare("SELECT * FROM likes WHERE article_id = ?");
-    //     $stmt->execute([$this->id]);
-    //     $results = $stmt->fetchAll();
-
-    //     foreach ($results as $row) {
-    //         $this->likes[] = new Like($row['likeId'], $row['article_id'], $row['userId'], $row['created_at']);
-    //     }
-    // }
-
-    // // Getter pour les commentaires
-    // public function getComments() {
-    //     return $this->comments;
-    // }
-
-    // // Getter pour les likes
-    // public function getLikes() {
-    //     return $this->likes;
-    // }
+    // Delete an article
+    public function delete() {
+        if ($this->id) {
+            $stmt = $this->cnx->prepare("DELETE FROM article WHERE art_Id = ?");
+            return $stmt->execute([$this->id]);
+        }
+        return false;
+    }
 }
