02d199ff62cf9b57921f447c0ebc5d2a693ffc44
Mahjoubech <charkaouielmahjoub50@gmail.com>
2025-01-11 17:54:05 +0100

    add class admin for manag user delet ar chang claint

--- class/admin.php
+++ class/admin.php
@@ -1,9 +1,27 @@
 <?php 
 require_once './User.php';
-class Client extends User{
+class Admin extends User{
+    private $id;
     public function __construct($cnx) {
         parent ::__construct($cnx);
     }
+    public function setId($id){
+        $this->id = $id;
+    }
+    public  function getId(){
+        return $this->id ;
+    }
+   public function DeletUser (){
+     $delet = $this->cnx->prepare('DELETE FROM user WHERE useId=?');
+     $delet->execute([$this->getId()]);
+ 
+     }
+     public function chngRole (){
+        $change = $this->cnx->prepare('update user set role_id=1 WHERE useId=?');
+        $change->execute([$this->getId()]);
     
+     }
 }
+
+
 ?>
