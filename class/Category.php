555d96ab470ed3b3df1bef68209a9612a844630c
Mahjoubech <charkaouielmahjoub50@gmail.com>
2025-01-11 19:08:46 +0100

    add class categiry

---
+++ class/Category.php
@@ -0,0 +1,40 @@
+<?php 
+class Category{
+    private $id;
+    private $name;
+    private $cnx;
+    public function __construct($db) {
+        $this->cnx = $db;
+    }
+    public function setId($id){
+        $this->id = $id;
+    }
+    public  function getId(){
+        return $this->id ;
+    }
+    public function setName($name){
+        $this->name = $name;
+    }
+    public  function getName(){
+        return $this->name ;
+    }
+   public function getAllCategory(){
+    $sql = $this->cnx->prepare('SELECT * from category order by catId Asc');
+        $sql->execute();
+       return $sql->fetchAll(PDO::FETCH_ASSOC);
+ 
+     }
+     public function chngRole (){
+        $change = $this->cnx->prepare('update user set role_id=1 WHERE useId=?');
+        $change->execute([$this->getId()]);
+    
+     }
+     public function addCategory(){
+      $sql = $this->cnx->prepare('INSERT INTO category(name) VALUES (?)');
+      $sql->execute([$this->getName()]);
+     }
+     
+}
+
+
+?>
